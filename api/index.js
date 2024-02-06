94671feaecb802948f190da35279bcc07427b07f
Abdelaziz <azizmahamat@gmail.com>
2024-02-06 18:27:57 +0100

    add auth route and controller

--- api/index.js
+++ api/index.js
@@ -2,9 +2,14 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
+
 dotenv.config();
 
 const app = express();
+
+app.use(express.json());
 
 mongoose
   .connect(process.env.MONGODB)
@@ -18,3 +23,6 @@ mongoose
 app.listen(3000, () => {
   console.log("server running on port 3000!...");
 });
+
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
